b8aec211b085562fda9adb2f6164046aa27c26c3
ws <1747709835@qq.com>
2021-12-12 14:46:22 +0800

    修改数据库配置

--- meiduo_mail/meiduo_mail/settings.py
+++ meiduo_mail/meiduo_mail/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'meiduo_mail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 引擎
+        'NAME': 'meiduo_mail',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',  # 测试服务器--->不可能由root操作--->删库操作--->只允许操作指定的数据库
+        'PASSWORD': 'kingshen2'
+
     }
 }
 
