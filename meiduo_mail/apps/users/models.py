629bd73648d083794847f61dac7fc33108f09910
ws <1747709835@qq.com>
2021-12-13 16:11:41 +0800

    为系统自带用户表更新字段并使用继承后的新表

--- meiduo_mail/apps/users/models.py
+++ meiduo_mail/apps/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+# 自己定义模型
+# 密码需要加密，登录的时候还要进行密码的验证
+# class User(models.Model):
+#     username = models.CharField(max_length=20, unique=True)
+#     password = models.CharField(max_length=20)
+#     mobile = models.CharField(max_length=11, unique=True)
+
+
+# ------>使用系统自带的auth_user表
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):   # 继承自AbstractUser类
+    mobile = models.CharField(max_length=11, unique=True)
+
+    class Meta:
+        db_table = 'tb_users'   # 设置表名字
+        verbose_name = '用户管理'   # 设定名称
+        verbose_name_plural = verbose_name  # 复数形式变为单数形式
+
+
+
+
+
+
+
+
--- meiduo_mail/apps/users/views.py
+++ meiduo_mail/apps/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+需求分析：根据页面的功能（从上倒下，从左到右面），那些功能需要和后段配合完成
+如何确定 那些功能需要和后段进行交互？？？
+    1.经验
+    2.关注类似网站的相似功能
+"""
+
