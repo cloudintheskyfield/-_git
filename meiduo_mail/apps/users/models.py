5ad9a8c2118eef7731848b5fa171d142523165bb
ws <1747709835@qq.com>
2021-12-26 23:28:39 +0800

    地址模型类的建立

--- meiduo_mail/apps/users/models.py
+++ meiduo_mail/apps/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 # 第一种方式：自己定义模型
 # 密码需要加密，登录的时候还要进行密码的验证
@@ -12,28 +11,45 @@ from django.db import models
 
 # ------>使用系统自带的auth_user表
 from django.contrib.auth.models import AbstractUser
+
+
 # from django.contrib.auth.models import User   .user继承自AbstractUser
 
 
-class User(AbstractUser):   # 继承自AbstractUser类
+class User(AbstractUser):  # 继承自AbstractUser类
     mobile = models.CharField(max_length=11, unique=True)
     email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+    # 新增默认地址字段
+    default_address = models.ForeignKey('Address', related_name='users', null=True, blank=True,
+                                        on_delete=models.SET_NULL, verbose_name='默认地址')
 
     class Meta:
-        db_table = 'tb_users'   # 设置表名字
-        verbose_name = '用户管理'   # 设定名称，后台管理系统显示
-        verbose_name_plural = verbose_name   # 复数形式变为单数形式
+        db_table = 'tb_users'  # 设置表名字
+        verbose_name = '用户管理'  # 设定名称，后台管理系统显示
+        verbose_name_plural = verbose_name  # 复数形式变为单数形式
 
 
+# --------------------------> 创建用户个人中心的数据表<--------------
+from utils.models import BaseModel
 
 
+class Address(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses',
+                                 verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses',
+                                 verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name='手机')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
+    is_deleted = models.BooleanField(default=False, verbose_name='逻辑删除')
 
-
-
-
-
-
-
-
-
-
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = '用户地址'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
