81a8346ef5ea4c7dedb595fe6bb943e1868703e3
ws <1747709835@qq.com>
2022-01-01 16:45:57 +0800

    详情页面的渲染实现

--- meiduo_mail/apps/goods/urls.py
+++ meiduo_mail/apps/goods/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.goods.views import IndexView, ListView, SKUSearchView
+from apps.goods.views import IndexView, ListView, SKUSearchView, DetailView
 urlpatterns = [
     # 商品首页
     path('index/', IndexView.as_view()),
@@ -7,6 +7,23 @@ urlpatterns = [
     path('list/<category_id>/skus/', ListView.as_view()),
     # 搜索/索引         没有as_view()直接实例化即可
     path('search/', SKUSearchView()),
+    #
+    path('detail/<sku_id>/', DetailView.as_view()),
 
 
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- meiduo_mail/apps/goods/views.py
+++ meiduo_mail/apps/goods/views.py
@@ -208,3 +208,58 @@ class SKUSearchView(SearchView):
             })
         # 这里后端需求的数据中，只需求了相关数据，并没有要求code和errmsg
         return JsonResponse(sku_list, safe=False)
+
+
+"""
+需求：详情页面
+    1.分类数据
+    2.面包屑
+    3.SKU信息
+    4.规格信息
+    
+    我们的详情也是静态化实现的，但是我们在讲解静态话之前，应该可以先把详情页面的数据展示出来
+"""
+# 最后一个为规格信息
+from utils.goods import get_categories, get_breadcrumb, get_goods_specs
+class DetailView(View):
+    def get(self, request, sku_id):
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            pass
+        # 1.分类数据
+        categories = get_categories()
+        # 2.面包屑
+        breadcrumb = get_breadcrumb(sku.category)
+        # 3.SKU信息
+        # 4.规格信息
+        good_specs = get_goods_specs(sku)
+        context={
+            'categories': categories,
+            'breadcrumb': breadcrumb,
+            'sku': sku,
+            'specs': good_specs,
+        }
+        return render(request, 'detail.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
